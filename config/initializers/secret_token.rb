f79eb9f4406dedcd08f3c661a908b0105d1326e9
tallbryan <b_d_williams@yahoo.com>
2013-01-15 21:40:59 -0800

    initialize new rails app zwb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zwb::Application.config.secret_token = 'c1a51e34815295f3fd1fb24ac34d694914bb3a4405c8b1e4e2bbb41cc4095b0ba2d2685a53a287e914b0baeade2341b75b79559f3ade46bfb60d40292626bc0e'
