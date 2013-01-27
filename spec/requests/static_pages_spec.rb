d78b103869f3676dfae41551b94a1503fb3ba8b5
Zekarias Araya <zekarias99@gmail.com>
2013-01-27 14:24:56 -0800

    Eliminate these sources of duplication by telling RSpec that page is the subject of the tests using subject { page}methode

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,56 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  subject { page }
 
   describe "Home page" do
-
     before { visit root_path }
 
-    it "should have h1 'ZWB'" do
-      page.should have_selector('h1', text:'ZWB')
-    end
-
-    it "should have title 'ZWB | Home' " do	
-      page.should have_selector('title', text: "ZWB | Home")
-    end
+    it { should have_selector('h1', text:'ZWB') }
+    it { should have_selector('title', text: "ZWB | Home") }
   end
 
   describe "Help page" do
-
     before { visit help_path }
 
-    it "should have h1 'Help'" do     
-      page.should have_selector('h1', text: 'Help')
-    end
-
-    it "shoul have title 'Help'" do
-      page.should have_selector('title', text: 'ZWB | Help')
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: 'ZWB | Help') }
   end
 
   describe "About Us page" do
-
     before { visit about_path }
 
-    it "should have h1 'About Us'" do     
-      page.should have_selector('h1', text: 'About Us')
-    end
-
-    it "should have title 'Invest-Globe | About Us'" do
-      page.should have_selector('title', text: 'ZWB | About Us')
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: 'ZWB | About Us') }
   end
 
   describe "Contact Us page" do
-
     before { visit contact_path }
 
-    it "should have h1 'Contact Us'" do     
-      page.should have_selector('h1', text: 'Contact Us')
-    end
-
-    it "should have title" do
-      page.should have_selector('title', text: 'ZWB | Contact Us')
-    end
+    it { should have_selector('h1', text: 'Contact Us') }
+    it { should have_selector('title', text: 'ZWB | Contact Us') }
   end
 end
