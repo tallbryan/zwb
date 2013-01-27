baa84511e918895394be24083a11df225554b3f3
Zekarias Araya <zekarias99@gmail.com>
2013-01-27 12:30:43 -0800

    Test for the named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Application page" do  
 
     it "should have link 'Home' " do
-      visit '/static_pages/home'  
+      visit root_path  
       page.should have_link("Home", :href => '#')
     end
 
     it "should have link 'Help' " do
-      visit '/static_pages/home'  
+      visit help_path  
       page.should have_link("Help", :href => '#')
     end
 
     it "should have link 'Sign up' " do
-      visit '/static_pages/home'  
+      visit root_path  
       page.should have_link("Sign up", :href => '#')
     end  
   end 
@@ -23,52 +23,52 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have h1 'ZWB'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'ZWB')
+      visit root_path
+      page.should have_selector('h1', text:'ZWB')
     end
 
     it "should have title 'ZWB | Home' " do
-      visit '/static_pages/home'	
-      page.should have_selector('title', :text => "ZWB | Home")
+      visit root_path	
+      page.should have_selector('title', text: "ZWB | Home")
     end
   end
 
   describe "Help page" do
 
     it "should have h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "shoul have title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => 'ZWB | Help')
+      visit help_path
+      page.should have_selector('title', text: 'ZWB | Help')
     end
   end
 
   describe "About Us page" do
 
     it "should have h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have title 'Invest-Globe | About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => 'ZWB | About Us')
+      visit about_path
+      page.should have_selector('title', text: 'ZWB | About Us')
     end
   end
 
   describe "Contact Us page" do
 
     it "should have h1 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact Us')
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact Us')
     end
 
     it "should have title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => 'ZWB | Contact Us')
+      visit contact_path
+      page.should have_selector('title', text: 'ZWB | Contact Us')
     end
   end
 end
