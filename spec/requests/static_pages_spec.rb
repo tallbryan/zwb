2684b07f3a693e389d755252eaefd4afb39d4822
Zekarias Araya <zekarias99@gmail.com>
2013-01-27 14:02:07 -0800

    Eliminate some duplication with a before block

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,52 +4,52 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    before { visit root_path }
+
     it "should have h1 'ZWB'" do
-      visit root_path
       page.should have_selector('h1', text:'ZWB')
     end
 
-    it "should have title 'ZWB | Home' " do
-      visit root_path	
+    it "should have title 'ZWB | Home' " do	
       page.should have_selector('title', text: "ZWB | Home")
     end
   end
 
   describe "Help page" do
 
-    it "should have h1 'Help'" do
-      visit help_path
+    before { visit help_path }
+
+    it "should have h1 'Help'" do     
       page.should have_selector('h1', text: 'Help')
     end
 
     it "shoul have title 'Help'" do
-      visit help_path
       page.should have_selector('title', text: 'ZWB | Help')
     end
   end
 
   describe "About Us page" do
 
-    it "should have h1 'About Us'" do
-      visit about_path
+    before { visit about_path }
+
+    it "should have h1 'About Us'" do     
       page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have title 'Invest-Globe | About Us'" do
-      visit about_path
       page.should have_selector('title', text: 'ZWB | About Us')
     end
   end
 
   describe "Contact Us page" do
 
-    it "should have h1 'Contact Us'" do
-      visit contact_path
+    before { visit contact_path }
+
+    it "should have h1 'Contact Us'" do     
       page.should have_selector('h1', text: 'Contact Us')
     end
 
     it "should have title" do
-      visit contact_path
       page.should have_selector('title', text: 'ZWB | Contact Us')
     end
   end
