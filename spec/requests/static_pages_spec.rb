7777626cfd6e7409cf324f3df777398a848aadf6
Zekarias Araya <zekarias99@gmail.com>
2013-01-27 19:20:20 -0800

    Test for the links on the layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,4 +32,12 @@ describe "Static pages" do
     it { should have_selector('h1', text: 'Contact Us') }
     it { should have_selector('title', text: full_title('Contact Us')) }
   end
+  it "should have the right link on the layout" do
+    visit root_path
+    click_link "About Us"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact Us"
+  end
 end
