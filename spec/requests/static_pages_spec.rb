59ebce159e3006d921a8f8473a78f77ba194565d
Zekarias Araya <zekarias99@gmail.com>
2013-01-27 18:27:56 -0800

    vv

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_selector('h1', text:'ZWB') }
-    it { should have_selector('title', text: "ZWB") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', :text => '| Home')} 
   end
 
@@ -16,20 +16,20 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: 'ZWB | Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About Us page" do
     before { visit about_path }
 
     it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: 'ZWB | About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact Us page" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: 'ZWB | Contact Us') }
+    it { should have_selector('title', text: full_title('Contact Us')) }
   end
 end
