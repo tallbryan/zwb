bec8b8f0a147a494e7fe97fc511a03aa5fa1e0ec
tallbryan <b_d_williams@yahoo.com>
2013-02-11 22:10:43 -0800

    play to see if @portfolio.name is there and put it in a flash notice

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,15 @@ class PortfoliosController < ApplicationController
   def new
     @portfolio = Portfolio.new
   end
+  
+  def create
+    @portfolio = Portfolio.new (params[:portfolio])
+    if @portfolio.save
+      flash[:notice] = "Portfolio has been created"
+      flash[:notice] = "#{@portfolio.name}"
+      redirect_to @portfolio
+    else
+      #nothing, yet
+    end
+  end
 end
