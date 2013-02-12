3da00b47affed827213f1e8689006bb66a522878
tallbryan <b_d_williams@yahoo.com>
2013-02-11 20:04:38 -0800

    add creating_portfolios_spec.rb

---
+++ spec/integration/creating_portfolios_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Adding a new portfolio' do
+  before do
+    visit '/'
+    click_link 'New Portfolio'
+  end
+
+  scenario 'Can create a new portfolio' do
+    fill_in 'Portfolio Name' :with => 'Value Portfolio'
+    click_button 'New Portfolio'
+    page.should have_content 'Your portfolio has been created'
+  end
+  
+end
