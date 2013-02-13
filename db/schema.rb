5efbe72ceadf688a60d992fb26c5540001963586
Zekarias Araya <zekarias99@gmail.com>
2013-02-13 08:08:44 -0800

    Generate migration add_index_to_users_email, validate the email format with a regular expression, add migration for enforcing email uniqueness, test for the rejection of duplicate email addresses and insensitive to case

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130131102315) do
+ActiveRecord::Schema.define(:version => 20130213153911) do
 
   create_table "stocks", :force => true do |t|
     t.string   "name"
@@ -22,5 +22,14 @@ ActiveRecord::Schema.define(:version => 20130131102315) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
