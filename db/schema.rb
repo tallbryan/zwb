afd6d6432745a5360fb87db867262901c90b6345
Zekarias Araya <zekarias99@gmail.com>
2013-02-13 10:20:30 -0800

    Add a secure password, add password_digest attribute test for the password and password confirmation to the User model, add test for the authenticate method test for the email downcasing also add bcrypt-ruby to the Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130213153911) do
+ActiveRecord::Schema.define(:version => 20130213163418) do
 
   create_table "stocks", :force => true do |t|
     t.string   "name"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(:version => 20130213153911) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
