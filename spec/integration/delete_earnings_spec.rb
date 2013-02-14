29e8866c5e28aadeee9c110bbdf543d06da14b5e
wsugg <sugg.will@gmail.com>
2013-02-13 23:47:56 -0800

    fixed deleting earnings bugs in spec

--- spec/integration/delete_earnings_spec.rb
+++ spec/integration/delete_earnings_spec.rb
@@ -1,18 +1,36 @@
 require 'spec_helper'
   
   feature 'Deleting Earnings' do
-   let!(:stock) { Factory(:stock) }
-   let!(:earning) { Factory(:earning, :stock => stock) }
-
     before do
-     visit '/'
-     click_link stock.name
-     click_link earning.title
-    end
+      visit '/stocks'
+      click_link 'New Stock'
+      fill_in 'Name', :with => 'Advanced Mirco Devices'
+      fill_in 'Symbol', :with => 'AMD'
+      fill_in 'Price', :with => '3.34'
+      fill_in 'Pe', :with => '12'
+      fill_in 'Div', :with => '1.23'
+      click_button 'Create Stock'
+      page.should have_content('Stock has been added.')
 
-   scenario "Deleting a ticket" do
-    click_link "Delete Ticket"
-    page.should have_content("Ticket has been deleted.")
-    page.current_url.should == project_url(project)
+      visit '/stocks'
+      click_link 'Advanced Mirco Devices'  
+      page.should have_content('Earnings') 
+      click_link' Add Earnings '
+      fill_in 'stock_earnings_attributes_0_year', :with => '2002'
+      fill_in 'stock_earnings_attributes_0_earnings', :with => '2.50'
+      fill_in 'stock_earnings_attributes_0_sheq', :with => '200'
+      fill_in 'stock_earnings_attributes_0_pe', :with => '120'
+      fill_in 'stock_earnings_attributes_0_div', :with => '2.23'
+      click_button "Update Earnings"
+      page.should have_content("Stock has been updated.")
+      end
+
+   scenario "Deleting an earning" do
+    visit '/stocks'
+    click_link 'Advanced Mirco Devices' 
+    click_link 'Edit Earnings'
+    check("stock_earnings_attributes_0__destroy")
+    click_button 'Update Earnings'
+    page.should have_content("Stock has been updated.")
    end
 end
