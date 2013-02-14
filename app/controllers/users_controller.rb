eedf90086032a9b0a7c055a94d0eb553335ac2e7
Zekarias Araya <zekarias99@gmail.com>
2013-02-13 20:37:28 -0800

    Finish the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  	  redirect_to root_path	
+  	  flash[:succcess] = "Welcome to ZWB"	
+  	  redirect_to @user	
   	else  
   	  render 'new'
   	end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,9 +28,18 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest') }
+      end
     end
 
     describe "with valid information" do
+
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -41,6 +50,23 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving a user" do
+        before { click_button submit }
+
+      	let(:user) { User.find_by_email('user@example.com') }
+      	
+      	it { should have_selector('title', text: user.name) }
+      end
     end
   end
 end
+
+
+
+
+
+
+
+
+
