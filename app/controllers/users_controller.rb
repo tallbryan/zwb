e44d17078a65e3e36a8cad83fdc1aa0fba6e0687
Zekarias Araya <zekarias99@gmail.com>
2013-02-15 15:02:01 -0800

    Add user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,17 @@ class UsersController < ApplicationController
   	  render 'new'
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a seccessful update.
+    else
+     render 'edit'
+    end 
+  end
 end
