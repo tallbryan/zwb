11b444a15ff4e454814961b1b7a84a9f9d6d5266
Zekarias Araya <zekarias99@gmail.com>
2013-02-15 10:26:08 -0800

    Custom matcher RSpec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,11 +18,11 @@ describe "Authentication" do
     before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message }
 
      describe "after visiting another page" do
       before { click_link "Home" }
-      xit { should_not have_selector('div.alert.alert-error') }
+      xit { should_not have_error_message }
     end 
 
     describe "with valid information" do
