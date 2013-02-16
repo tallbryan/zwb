0a807ef7128c507861d5aec8eb95d27b80553d6a
Zekarias Araya <zekarias99@gmail.com>
2013-02-15 18:42:04 -0800

    Add testing that the edit and update actions are protected

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,11 +27,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_selector('title',   text: user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
