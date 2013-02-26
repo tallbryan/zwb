9215cc9aab3347ba2181d1f9c5db3c5849339516
tallbryan <b_d_williams@yahoo.com>
2013-02-26 00:11:32 -0800

    add more defaults to stock_factory

--- spec/integration/seeds_spec.rb
+++ spec/integration/seeds_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 feature "Seed Data" do
 	scenario "The basics"
-	load Rails.root + db/seeds.rb
+	load Rails.root + "db/seeds.rb"
 end
--- spec/support/factories/stock_factory.rb
+++ spec/support/factories/stock_factory.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
 	factory :stock do
 	symbol "AMD"
+	name "Advanced Micro Devices"
+	pe 15
+	price 5
+	div 2.00
 	end
 end
